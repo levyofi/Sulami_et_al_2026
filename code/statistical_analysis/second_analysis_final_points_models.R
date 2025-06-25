f3ae8b8cfcc44f350933c8a58ff1b6646b95da8b
Ofir Levy <levyofi@gmail.com>
2025-06-25 23:22:12 +0300

    Update second_analysis_final_points_models.R

--- code/statistical_analysis/second_analysis_final_points_models.R
+++ code/statistical_analysis/second_analysis_final_points_models.R
@@ -15,7 +15,7 @@ summary(model_solar)
 p = predict_response(model_solar, terms = c("size", "value[80, 400, 800]"))
 df_p_solar =as.data.frame(p)
 names(df_p_solar) = c("size", "predicted", "std.error", "conf.low", "conf.high", "solar")
-save(df_p_solar, file = "predicted_solar.RData")
+save(df_p_solar, file = "code/Figures/predicted_solar.RData")
 
 # TGI
 #effect of solar radiation and size - see points_stats_TGI.R for model selection
@@ -25,7 +25,7 @@ summary(model_TGI)
 p = predict_response(model_TGI, terms = c("size", "value[-0.03, 0, 0.03]"))
 df_p_TGI =as.data.frame(p)
 names(df_p_TGI) = c("size", "predicted", "std.error", "conf.low", "conf.high", "TGI")
-save(df_p_TGI, file = "predicted_TGI.RData")
+save(df_p_TGI, file = "code/Figures/predicted_TGI.RData")
 
 # skyview
 #effect of solar radiation and size - see points_stats_skyview.R for model selection
@@ -35,7 +35,7 @@ summary(model_skyview)
 p = predict_response(model_skyview, terms = c("size", "value[0.2, 0.5, 0.8]"))
 df_p_skyview =as.data.frame(p)
 names(df_p_skyview) = c("size", "predicted", "std.error", "conf.low", "conf.high", "skyview")
-save(df_p_skyview, file = "predicted_skyview.RData")
+save(df_p_skyview, file = "code/Figures/predicted_skyview.RData")
 
 
 #effect of solar radiation and size - see points_stats_slope_sd.R for model selection
@@ -45,5 +45,5 @@ summary(model_slope_sd)
 p = predict_response(model_slope_sd, terms = c("size", "value [0.5, 5, 9.5]"), condition = c(x = mean(slope_sd_data$x), y = mean(slope_sd_data$y)))
 df_p_slope =as.data.frame(p)
 names(df_p_slope) = c("size", "predicted", "std.error", "conf.low", "conf.high", "slope_SD")
-save(df_p_slope, file = "predicted_solar_sd.RData")
+save(df_p_slope, file = "code/Figures/predicted_solar_sd.RData")
 
